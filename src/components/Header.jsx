2f4fae1a2685f012212c10482a4f9f6ef4685571
ilnur.ahm <axmetzhanovi@mail.ru>
2024-02-01 14:08:50 +0300

    getting started with git

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import s from './Header.module.css'
+
+const Header = () => {
+    return <header className={s.header}>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/5/59/Sketch_Logo.svg' />
+    </header>
+}
+
+export default Header; 
